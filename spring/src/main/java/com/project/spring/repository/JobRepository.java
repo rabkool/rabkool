2eb57244bb7ad4422af652dbf980fe29b883881c
90595 <90595@192.168.3.133>
2019-03-04 19:36:24 +0800

    qwe

---
+++ spring/src/main/java/com/project/spring/repository/JobRepository.java
@@ -0,0 +1,19 @@
+package com.project.spring.repository;
+
+import com.project.spring.domain.Job;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
+
+/**
+ * @author wangheng
+ * @create 2019-02-25 下午5:40
+ * @desc
+ **/
+public interface JobRepository extends MongoRepository<Job, String> {
+
+    @Query(value = "{\"jobId\":{\"$regex\":?0}}")
+    Optional<Job> findByjobId(String jobId);
+
+}
